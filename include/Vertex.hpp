bc040faddb6b9b84029feb82245e4c16bef4b859
Roman Khomenko <romankhomenko1995@gmail.com>
2018-01-13 18:30:20 +0300

    Now Vertex is POD type

--- include/Vertex.hpp
+++ include/Vertex.hpp
@@ -12,47 +12,34 @@
 #include <eigen3/Eigen/Dense>
 #endif
 
-template <typename T, typename U>
-constexpr size_t offsetofImpl(T const* t, U T::*a) {
-    return reinterpret_cast<char const*>(t) -
-                       reinterpret_cast<char const*>(&(t->*a)) >=
-                   0
-               ? reinterpret_cast<char const*>(t) -
-                     reinterpret_cast<char const*>(&(t->*a))
-               : reinterpret_cast<char const*>(&(t->*a)) -
-                     reinterpret_cast<char const*>(t);
-}
-
-#define offsetOf(Type_, Attr_) \
-    offsetofImpl((Type_ const*)nullptr, &Type_::Attr_)
+#include <array>
 
 class Vertex {
 public:
-    using Vec4 = Eigen::Matrix<float, 1, 4>;
-    using PositionType = Vec4;
-    using ColorType = Vec4;
-
     using FloatType = float;
     using IntType = std::int32_t;
+
+    using Vec4 = Eigen::Matrix<float, 1, 4>;
+    using PODVec4 = std::array<FloatType, 4>;
+    using PositionType = PODVec4;
+    using ColorType = PODVec4;
 
     Vertex() : Vertex{0.0, 0.0, 0.0, 1.0} {}
     Vertex(FloatType x, FloatType y) : Vertex{x, y, 0.0, 1.0} {}
     Vertex(FloatType x, FloatType y, FloatType z) : Vertex{x, y, z, 1.0} {}
     Vertex(FloatType x, FloatType y, FloatType z, FloatType h)
-        : Position{x, y, z, h} {}
-    Vertex(const PositionType& position) : Position{position} {}
-    Vertex(const PositionType& position, const ColorType& color)
-        : Position{position}, Color{color} {}
+        : Position{{x, y, z, h}} {}
+    Vertex(const Vec4& position) : Position{ToPODVec4(position)} {}
+    Vertex(const Vec4& position, const Vec4& color)
+        : Position{ToPODVec4(position)}, Color{ToPODVec4(color)} {}
 
     Vertex(Vertex&& v) = default;
     Vertex(const Vertex& v) = default;
 
-    void SetColor(const ColorType& color) noexcept { Color = color; }
+    void SetColor(const Vec4& color) noexcept { Color = ToPODVec4(color); }
 
-    constexpr const PositionType& GetPosition() const noexcept {
-        return Position;
-    }
-    constexpr const ColorType& GetColor() const noexcept { return Color; }
+    Vec4 GetPosition() const noexcept { return ToVec4(Position); }
+    Vec4 GetColor() const noexcept { return ToVec4(Color); }
 
     static constexpr IntType GetPositionTupleSize() noexcept {
         return POSITION_TUPLE_SIZE;
@@ -61,21 +48,27 @@ public:
         return COLOR_TUPLE_SIZE;
     }
     static constexpr IntType GetPositionOffset() noexcept {
-        return offsetOf(Vertex, Position);
+        return offsetof(Vertex, Position);
     }
     static constexpr IntType GetColorOffset() noexcept {
-        return offsetOf(Vertex, Position);
+        return offsetof(Vertex, Position);
     }
     static constexpr IntType GetStride() noexcept { return sizeof(Vertex); }
 
 private:
-    static const IntType POSITION_TUPLE_SIZE = 4;
+    static const IntType POSITION_TUPLE_SIZE = 3;
     static const IntType COLOR_TUPLE_SIZE = 4;
+
+    static Vec4 ToVec4(const PODVec4& vec) {
+        return Vec4(vec[0], vec[1], vec[2], vec[3]);
+    }
+
+    static PODVec4 ToPODVec4(const Vec4& vec) {
+        return {{vec[0], vec[1], vec[2], vec[3]}};
+    }
 
     PositionType Position;
     ColorType Color;
 };
-
-#undef offsetOf
 
 #endif  // CG_LAB_VERTEX_HPP_
